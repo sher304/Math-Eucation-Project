fc80e62e561edee28ef1044df348fe86656f7549
sher304 <esherovshermat@gmail.com>
2023-05-12 01:35:09 +0600

    💄 Change Quiz Page

--- Math Education Project/Extension/Extension.swift
+++ Math Education Project/Extension/Extension.swift
@@ -7,6 +7,37 @@
 
 import UIKit
 
+class GradientView: UIView {
+
+    private let gradient : CAGradientLayer = CAGradientLayer()
+    private let gradientStartColor: UIColor
+    private let gradientEndColor: UIColor
+
+    init(gradientStartColor: UIColor, gradientEndColor: UIColor) {
+        self.gradientStartColor = gradientStartColor
+        self.gradientEndColor = gradientEndColor
+        super.init(frame: .zero)
+    }
+
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        gradient.frame = self.bounds
+    }
+
+    override public func draw(_ rect: CGRect) {
+        gradient.frame = self.bounds
+        gradient.colors = [gradientEndColor.cgColor, gradientStartColor.cgColor]
+        gradient.startPoint = CGPoint(x: 0.85, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 0)
+        if gradient.superlayer == nil {
+            layer.insertSublayer(gradient, at: 0)
+        }
+    }
+}
+
+
 extension UIView {
     func addBottomShadow() {
         backgroundColor = .white
@@ -16,17 +47,6 @@ extension UIView {
         layer.shadowColor = UIColor.gray.cgColor
         layer.shadowOffset = CGSize(width: 0 , height:2)
     }
-    
-    
-//    func addTopShadow(shadowColor : UIColor, shadowOpacity : Float,shadowRadius : Float, offset:CGSize){
-//        self.layer.shadowColor = shadowColor.cgColor
-//        self.layer.shadowOffset = offset
-//        self.layer.shadowOpacity = shadowOpacity
-//        self.layer.shadowRadius = CGFloat(shadowRadius)
-//        self.clipsToBounds = false
-        
-        
-//    }
     
     func addTopShadow(){
         backgroundColor = .white
